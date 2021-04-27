2868bb49b5d6a2dee0bc29d584834a5f58c3f028
Juan Gramajo Perez <>
2021-04-27 15:19:14 +0000

    for loop to only show the questions which had zero responses

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import requests
 response = requests.get('https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow')
 
 for questions in response.json()['items']:
-  print(questions['title'])
-  print(questions['link'])
-  print()
+  if questions['answer_count'] == 0:
+    print(questions['title'])
+    print(questions['link'])
+  else:
+    print('Skipped')
+    print()
